7f6df3727d1e7628349ea74464d78ba108df0166
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2024-03-17 23:39:14 +0430

    Working with components styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css'
+  styles: [
+    `
+      h1 {
+        color: blue;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
