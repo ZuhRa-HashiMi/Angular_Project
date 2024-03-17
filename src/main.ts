ecad43bb1a7b6861fd17fc94defd82964b4398d8
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2024-03-17 15:13:36 +0430

    Start Angular Project

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppConfigModule } from './app/app.config';
+
+platformBrowserDynamic().bootstrapModule(AppConfigModule)
+  .catch(err => console.error(err));
