2b66e1afc9d136846780eb9f3e0446e4d6dc0f83
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2024-03-18 14:21:59 +0430

    Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,9 +10,14 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = "No server was created";
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server created successfully";
   }
 }
