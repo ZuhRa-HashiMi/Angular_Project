858781f6647ad08e62925bf0383c9dcb2f1b76c2
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2024-03-18 14:01:06 +0430

    Property dataBinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,9 +4,15 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]', // select as attributes
   // selector: '.app-servers', // select as class
   selector: 'app-servers',
-  template: `<app-server></app-server>
-                <app-server></app-server>`,
+  // template: `<app-server></app-server>
+  //               <app-server></app-server>`,
+  templateUrl: './servers.component.html',
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
