9e248b31f53e322335f8bc6e5e6acf5aeb66feb6
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2024-03-22 15:25:50 +0430

    Styling Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,6 +8,16 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+
+
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
   getServerStatus() { // definition the method to get the status
     return this.serverStatus;
   }
